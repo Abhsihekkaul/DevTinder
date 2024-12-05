f11cae1c25d6a208f39d4cad73d532532fba6fd3
Abhishek kaul <Abhishekkaul32@gmail.com>
2024-12-05 14:26:38 +0530

    Playing with app.use and next function and understanding all the error which may or may not occur along the way

--- app.js
+++ app.js
@@ -1,30 +1,27 @@
 const express = require('express'); // Importing the Express library
 const app = express(); // Creating an instance of the Express application
 
-// Define a route for the /user endpoint using the GET method
-app.get('/user', (req, res) => {
-    res.send("Welcome to the User page!"); // Send a response for GET requests to /user
-});
+app.use('/User1', 
+    (req,res,next)=>{
+        console.log("I am the first console")
+        next();
+        res.send("I am the first Response Bro!")
+    },
+    (req,res,next)=>{
+        console.log("I am the second console")
+        next();
+    },
+    (req,res,next)=>{
+        console.log("I am the third Console")
+        next();
+    },
+    (req,res)=>{
+        console.log("I am the Fourth Console")
+    }
+)
 
-// Define a route for the /greet endpoint using the POST method
-app.post('/greet', (req, res) => {
-    res.send("Hello from India and Abhishek! Nice to meet you!"); // Send a response for POST requests to /greet
-});
 
-// Define a route for the /delete endpoint using the DELETE method
-app.delete('/delete', (req, res) => {
-    res.send("User deleted successfully!"); // Send a response for DELETE requests to /delete
-});
 
-// Middleware to handle requests to /special
-app.use('/special', (req, res) => {
-    res.send("You have reached the special page!"); // Send a response for any request to /special
-});
-
-// Middleware to handle all other unmatched routes
-app.use('/', (req, res) => {
-    res.send("Welcome to the homepage!"); // Send a default response for unmatched routes
-});
 
 // Start the server and listen on port 3000
 app.listen(3000, () => {
