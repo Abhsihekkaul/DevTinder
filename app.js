c1a815c83169633f7bb2046e7c944bc0e3a88da7
Abhishek kaul <Abhishekkaul32@gmail.com>
2024-12-05 13:32:16 +0530

    Learned how to use CRUD operation and postman

--- app.js
+++ app.js
@@ -1,11 +1,32 @@
-const express = require('express');
-const app = express();
+const express = require('express'); // Importing the Express library
+const app = express(); // Creating an instance of the Express application
 
-
-app.use((req,res)=>{
-    res.send("Hello from the India and from Abhishek as well, Por Favor, Hello Mate");
+// Define a route for the /user endpoint using the GET method
+app.get('/user', (req, res) => {
+    res.send("Welcome to the User page!"); // Send a response for GET requests to /user
 });
 
-app.listen(3000,()=>{
-    console.log("Hello world");
-})
+// Define a route for the /greet endpoint using the POST method
+app.post('/greet', (req, res) => {
+    res.send("Hello from India and Abhishek! Nice to meet you!"); // Send a response for POST requests to /greet
+});
+
+// Define a route for the /delete endpoint using the DELETE method
+app.delete('/delete', (req, res) => {
+    res.send("User deleted successfully!"); // Send a response for DELETE requests to /delete
+});
+
+// Middleware to handle requests to /special
+app.use('/special', (req, res) => {
+    res.send("You have reached the special page!"); // Send a response for any request to /special
+});
+
+// Middleware to handle all other unmatched routes
+app.use('/', (req, res) => {
+    res.send("Welcome to the homepage!"); // Send a default response for unmatched routes
+});
+
+// Start the server and listen on port 3000
+app.listen(3000, () => {
+    console.log("Server is running on http://localhost:3000");
+});
